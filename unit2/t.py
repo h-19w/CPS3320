7abeb67700bf83d1f188e44baa9cc19667547348
h-19w <167726843+h-19w@users.noreply.github.com>
2026-03-05 12:56:14 +0800

    unit 3 more

--- unit2/t.py
+++ unit2/t.py
@@ -2,7 +2,9 @@ import turtle
 
 turtle.pensize(5)
 turtle.speed(30)
-
+turtle.penup() 
+turtle.goto(0, -300)
+turtle.pendown()
 turtle.pencolor("red") # change pen color to red 
 turtle.circle(30)
 turtle.right(90)
@@ -21,6 +23,10 @@ t = turtle.Turtle()
 t.goto(0, 100)
 t.goto(-100, 0)
 t.goto(0, 0)
+t.goto(100, 0)
+t.goto(0, -100)
+t.goto(0, 0)
+
 
 
 turtle.done()
