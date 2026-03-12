cb8db5b5cd8608f117850e2f8cba5c4e583c54c4
h-19w <167726843+h-19w@users.noreply.github.com>
2026-03-12 12:10:57 +0800

    calorie loop counting

---
+++ calorie_forloop.py
@@ -0,0 +1,14 @@
+# calories_per_minute = 4.2
+# for i in range(10, 30, 5):
+#     print(f"{i}:{i * calories_per_minute} calories")
+
+# for x in range(10, 22, 2):
+    # print(x/10.0, end=" ")
+
+
+#          starting number, ending number, step
+for x in range(10, 22, 2):
+    print(x, end=" ")
+
+
+
---
+++ calorie_whileloop.py
@@ -0,0 +1,10 @@
+
+#uses a loop to display the number of calories burned after 10, 15, 20, 25, and 30 minutes.
+
+x = 10 
+while x<=30:
+    calories_burned = x * 4.2
+    print ("After " + str(x) + " minutes, you have burned " + str(calories_burned) + " calories.")
+    if x == 30:
+        break
+    x = x+5
