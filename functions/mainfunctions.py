bb353fde085d41ce6f697fc30ad7d3136388efd4
h-19w <167726843+h-19w@users.noreply.github.com>
2026-03-19 12:05:28 +0800

    ch 5 functions - main function

---
+++ functions/mainfunctions.py
@@ -0,0 +1,13 @@
+def main():
+    print('i have a message for u.')
+    message()
+    message2()
+    print('i cannot help u, goodbye!')
+
+def message():
+    print("i am Authur")
+    print("king of the knights of the round table")
+
+def message2():
+    print("i am the one who will save the world")
+main()
