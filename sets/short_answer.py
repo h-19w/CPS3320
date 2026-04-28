18a50efae0ce0188503512186733f135eba6d5da
h-19w <167726843+h-19w@users.noreply.github.com>
2026-04-28 11:55:29 +0800

    set [end]

---
+++ sets/short_answer.py
@@ -0,0 +1,15 @@
+def main ():
+    set1 = set('saturn')
+    set2 = {10}
+    set3 = set('a bb ccc dddd')
+    set4 = set([2, 4, 4, 6, 6, 6, 6])
+    set5 = set(['a', 'bb', 'cccc', 'ddddd'])
+
+    print(set1)
+    print(set2) 
+    print(set3)
+    print(set4)
+    print(set5)
+
+if __name__ == "__main__":
+    main()
