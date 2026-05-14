fccac969f53e0577cc1d94311fadb9e13294963e
h-19w <167726843+h-19w@users.noreply.github.com>
2026-05-14 12:58:04 +0800

    unit 14 start

---
+++ unit 14/dbm_intro.py
@@ -0,0 +1,9 @@
+# SQL 
+
+import sqlite3
+# is so light that is included in the Python standard library
+# no need to install anything extra
+# it is a file-based database, so you can create a database by creating a file
+# it is a relational database, so you can create tables and relationships between them
+# it is a SQL database, so you can use SQL commands to interact with it
+
